941008558762f9a3105e5a9e8a40aae1efaefbe8
Alexandrovna7 <dina25alexandrovna05@gmail.com>
2024-08-02 12:41:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,40 @@ form.addEventListener("submit", (event) => {
   // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
 
-  // Здесь твой код
+  const name = document.querySelector("#name");
+  const secondName = document.querySelector("#secondName");
+  const phone = document.querySelector("#phone");
+  const email = document.querySelector("#email");
+  const agree = document.querySelector("#agree");
+
+  fetch("https://polinashneid…;, {
+    method: "POST",
+    headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: "Bearer: Alexandrovna7",
+  },
+      body: JSON.stringify({
+      name: name.value,
+      secondName: secondName.value,
+      phone: phone.value,
+      email: email.value,
+      agree: agree.checked,
+    }),
+})
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    if (data.message) {
+      alert(data.message);
+    }
+    form.reset();
+  })
+  .catch((e) => {
+    alert("Произошла ошибка :(");
+  })
 });
+});
+
+
